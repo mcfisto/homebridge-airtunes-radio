c089ff18e4587949e719bed4e01052be459e0672
mcfisto <daniel@man.eu.com>
2019-01-12 23:40:39 +0100

    Fix

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const HomePodRadioPlatform = require('./lib/HomePodRadioPlatform');
 
-let Service, Characteristic;
+
+const HomePodRadioPlatformModule = require('./lib/HomePodRadioPlatform');
+const HomePodRadioPlatform = HomePodRadioPlatformModule.HomePodRadioPlatform
 
 module.exports = function (homebridge) {
+    HomePodRadioPlatform.setHomebridge(homebridge)
     homebridge.registerAccessory("homebridge-homepod-radio", "HomePodRadio", HomePodRadioPlatform);
 }
